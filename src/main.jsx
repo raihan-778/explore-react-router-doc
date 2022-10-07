80fbb459219a6c0f23d8c986699aa5eb16f610fb
raihan-778 <raihanarif778@gmail.com>
2022-10-07 07:58:07 +0600

    some data added

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
+import ErrorPage from "./error-page";
 import "./index.css";
+import Contact from "./routes/contact";
+import Root from "./routes/root";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
